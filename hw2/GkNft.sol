c4fcccae1bc95903a8dbb38b8ea9ac65539331db
zwfw <Cloud@TAIJI123!@#>
2025-10-11 08:46:23 +0800

    upd

---
+++ hw2/GkNft.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GkNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address public owner;
+
+    constructor() ERC721("GkNFT", "GNFT") {
+        owner = msg.sender;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
+        require(msg.sender == owner, "Only owner can mint");
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+         _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+}
